9a84c55d99380483133cb3f0bf863c4b1076d952
MoonFeather <moonbite233@gmail.com>
2025-06-26 21:20:06 +0800

    fix CMake FetchContent

--- cmake/OneMotorFindHyCAN.cmake
+++ cmake/OneMotorFindHyCAN.cmake
@@ -2,6 +2,7 @@ find_package(HyCAN QUIET)
 
 if (NOT HyCAN_FOUND)
     message(STATUS "HyCAN can't be found locally, try fetching from Github...")
+    include(FetchContent)
     FetchContent_Declare(
             HyCAN_fetched
             GIT_REPOSITORY "https://github.com/RoboMaster-DLMU-CONE/HyCAN"
