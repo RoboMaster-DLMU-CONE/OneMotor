fa8cb7e321fb7d7d2e8ad8e2ef2cfc8a31404d9b
moonfeather <moonbite233@gmail.com>
2025-07-09 23:51:14 +0800

    update test path to "tests"

--- cmake/OneMotorTests.cmake
+++ cmake/OneMotorTests.cmake
@@ -1,5 +1,5 @@
 enable_testing()
-add_executable(OneMotorTest_PID ${PROJECT_SOURCE_DIR}/test/PID_Latency.cpp)
+add_executable(OneMotorTest_PID ${PROJECT_SOURCE_DIR}/tests/PID_Latency.cpp)
 target_link_libraries(OneMotorTest_PID PRIVATE OneMotor)
 
 add_test(
