bcc2528e8b677c357277a7f4aad287c5f62ceda3
MoonFeather <moonbite233@gmail.com>
2025-08-26 22:24:38 +0800

    update example

--- example/DJI/M3508_Positional.cpp
+++ example/DJI/M3508_Positional.cpp
@@ -15,23 +15,23 @@ using OneMotor::Motor::DJI::M3508;
 using enum OneMotor::Motor::DJI::MotorMode;
 
 static constexpr PID_Params<float> POS_DEFAULT_PARAMS{
-    .Kp = 6,
+    .Kp = 3,
     .Ki = 0,
     .Kd = 0,
-    .MaxOutput = 2000,
-    .Deadband = 30,
+    .MaxOutput = 3000,
+    .Deadband = 100,
     .IntegralLimit = 500,
 };
 static constexpr PID_Params<float> ANG_DEFAULT_PARAMS{
-    .Kp = 5,
-    .Ki = 0,
+    .Kp = 1,
+    .Ki = 0.1,
     .Kd = 0,
-    .MaxOutput = 15000,
-    .Deadband = 30,
-    .IntegralLimit = 1500,
+    .MaxOutput = 8000,
+    .Deadband = 100,
+    .IntegralLimit = 500,
 };
 
-[[noreturn]] int main()
+int main()
 {
     CanDriver driver("can0");
     (void)driver.open()
@@ -53,7 +53,7 @@ static constexpr PID_Params<float> ANG_DEFAULT_PARAMS{
     while (true)
     {
         std::string param_to_change;
-        std::cout << "Enter parameter to change (ref, kp, kd, ki): ";
+        std::cout << "Enter parameter to change (ref, kp, kd, ki) or 'exit': ";
         std::cin >> param_to_change;
 
         if (param_to_change == "ref")
@@ -63,6 +63,10 @@ static constexpr PID_Params<float> ANG_DEFAULT_PARAMS{
             std::cin >> ref;
             m1.setPosRef(ref);
         }
+        else if (param_to_change == "exit")
+        {
+            break;
+        }
         else
         {
             std::cout << "Invalid parameter." << std::endl;
@@ -70,4 +74,5 @@ static constexpr PID_Params<float> ANG_DEFAULT_PARAMS{
     }
 
     (void)driver.close();
+    return 0;
 }
