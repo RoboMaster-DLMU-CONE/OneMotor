7f6655f9dc42f7c80b8f6222da2ff5aee83981a5
MoonFeather <moonbite233@gmail.com>
2026-01-12 17:06:35 +0800

    update J4310 example

--- example/DM/J4310.cpp
+++ example/DM/J4310.cpp
@@ -1,5 +1,7 @@
 #include <OneMotor/Motor/DM/DmMotor.hpp>
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include "OneMotor/Motor/DM/DmPolicy.hpp"
 #include "OneMotor/Motor/DM/DmTraits.hpp"
@@ -7,15 +9,17 @@
 
 using namespace OneMotor::Units::literals;
 using OneMotor::Motor::DM::J4310;
-using OneMotor::Motor::DM::J4310_PosVel;
+using OneMotor::Motor::DM::J4310_MIT;
 
 constexpr float cycle = 2 * std::numbers::pi;
 
-int main() {
+int main()
+{
     OneMotor::Can::CanDriver driver("can0");
     // Remember to Change the ID
-    J4310_PosVel j4310(driver, 0x53, 0x43);
-    (void)j4310.setPidParams(0.01f, 0, 0.05f);
+    J4310_MIT j4310(driver, 0x01, 0x11);
+    (void)j4310.setPidParams(0.5f, 0, 1.0f);
+    OneMotor::Thread::sleep_for(std::chrono::seconds(4));
 
     (void)j4310.enable();
     OneMotor::Thread::sleep_for(std::chrono::seconds(4));
@@ -25,14 +29,15 @@ int main() {
     // Position Velocity mode
 
     // MIT mode
-    (void)j4310.setRefs(360 * deg, 2 * rad / s, 0.01 * N * m);
+    (void)j4310.setRefs(0 * deg, 0 * rad / s, 0 * N * m);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     std::cout << j4310.getStatus().value().format() << std::endl;
     OneMotor::Thread::sleep_for(std::chrono::seconds(2));
     std::cout << j4310.getStatus().value().format() << std::endl;
     OneMotor::Thread::sleep_for(std::chrono::seconds(2));
 
-    (void)j4310.disable();
+    // (void)j4310.disable();
     OneMotor::Thread::sleep_for(std::chrono::seconds(2));
 
     return 0;
