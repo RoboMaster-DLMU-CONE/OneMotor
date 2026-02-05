4cfacba522771671c82f88bfa50917c2db5a624a
WindWeaver <moonbite233@gmail.com>
2026-02-05 17:55:47 +0800

    fix OnePID, mp-units build logic

--- cmake/OneMotorFindOnePid.cmake
+++ cmake/OneMotorFindOnePid.cmake
@@ -1,9 +1,11 @@
-if(NOT OnePID::OnePID)
-  FetchContent_Declare(
+find_package(OnePID QUIET)
+
+if (NOT OnePID_FOUND AND NOT OnePID::OnePID)
+    FetchContent_Declare(
             OnePID
             GIT_REPOSITORY https://gitee.com/dlmu-cone/OnePID
             GIT_TAG main
             GIT_SHALLOW true
     )
-  FetchContent_MakeAvailable(OnePID)
-endif()
+    FetchContent_MakeAvailable(OnePID)
+endif ()
